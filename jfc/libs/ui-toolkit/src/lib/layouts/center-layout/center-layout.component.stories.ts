476c4455141f8f9c425596a6b25be0f0f4e43c79
Joe Chapman <joe.chapman3737@gmail.com>
2023-05-23 18:56:41 +1200

    Centered layout

--- jfc/libs/ui-toolkit/src/lib/layouts/center-layout/center-layout.component.stories.ts
+++ jfc/libs/ui-toolkit/src/lib/layouts/center-layout/center-layout.component.stories.ts
@@ -2,13 +2,42 @@ import { Meta } from '@storybook/angular';
 import { CenterLayoutComponent } from './center-layout.component';
 
 export default {
-  title: 'CenterLayoutComponent',
+  title: 'Layouts / Center',
   component: CenterLayoutComponent,
+  argTypes: {
+    width: {
+      control: 'text',
+    },
+    backgroundColor: {
+      control: 'color',
+    },
+    contentBackgroundColor: {
+      control: 'color',
+    },
+  },
 } as Meta<CenterLayoutComponent>;
 
-export const Primary = {
-  render: (args: CenterLayoutComponent) => ({
-    props: args,
-  }),
-  args: {},
+export const Default = (args: CenterLayoutComponent) => ({
+  component: CenterLayoutComponent,
+  template: `<jfc-center-layout>{{content}}</jfc-center-layout>`,
+  props: {
+    ...args,
+    content: 'Something',
+  },
+});
+
+export const Editable = (args: CenterLayoutComponent) => ({
+  component: CenterLayoutComponent,
+  props: args,
+  template: `<jfc-center-layout
+    [width]="width"
+    [backgroundColor]="backgroundColor"
+    [contentBackgroundColor]="contentBackgroundColor"
+    >{{content}}</jfc-center-layout>`,
+});
+Editable.args = {
+  width: '400px',
+  backgroundColor: 'grey',
+  contentBackgroundColor: 'whitesmoke',
+  content: 'Something',
 };
--- jfc/libs/ui-toolkit/src/lib/layouts/center-layout/center-layout.component.ts
+++ jfc/libs/ui-toolkit/src/lib/layouts/center-layout/center-layout.component.ts
@@ -1,17 +1,44 @@
 import {
   ChangeDetectionStrategy,
   Component,
-  ViewEncapsulation,
+  HostBinding,
+  Input,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+/**
+ * Layout component that centers its content
+ */
 @Component({
   selector: 'jfc-center-layout',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './center-layout.component.html',
   styleUrls: ['./center-layout.component.scss'],
-  encapsulation: ViewEncapsulation.None,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class CenterLayoutComponent {}
+export class CenterLayoutComponent {
+
+  /**
+   * Width of the content
+   */
+  @Input()
+  public width: string = '400px';
+
+  /**
+   * Background color of the side bars
+   */
+  @Input()
+  public backgroundColor: string = 'black';
+
+  /**
+   * Background color of the content
+   */
+  @Input()
+  public contentBackgroundColor: string = 'white';
+
+  @HostBinding('style.backgroundColor') get getBackgroundColor() {
+    return this.backgroundColor;
+  }
+
+}
