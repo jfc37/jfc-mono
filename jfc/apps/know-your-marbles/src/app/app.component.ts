d9de7d3e24f146fe6db799fd5f7324e47420cc06
Joe Chapman <joe.chapman3737@gmail.com>
2023-05-23 22:01:40 +1200

    Use centered layout in kym

--- jfc/apps/know-your-marbles/src/app/app.component.ts
+++ jfc/apps/know-your-marbles/src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { CenterLayoutComponent } from '@jfc/ui-toolkit';
 
 @Component({
   standalone: true,
   selector: 'jfc-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  imports: [CenterLayoutComponent],
 })
-export class AppComponent {
-  title = 'know-your-marbles';
-}
+export class AppComponent {}
